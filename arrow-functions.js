d6c5a9c43988671b0dba0d8fe24ce4e57a5cf482
Lukas kostrubiec <lukasz.kostrubiec@gmail.com>
2024-01-23 00:05:01 +0000

    Theory and practice

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,44 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbners(a, b) {
+    // Code Block
+    return a + b;
+}
+
+// //To use the function we call it
+// We set up a varaible to store the output of the function
+let sum = addTwoNumbners(3, 5)
+console.log(sum)
 
 // Arrow Function With Parameters
+const addTwoNumbners2 = (a, b) => {
+    // Code Block
+    return a + b;
+}
+//To use the function we call it
+let sum2 = addTwoNumbners2(3, 5)
+console.log(sum2)
 
 // Single Line Arrow Function With Parameters
+//const addTwoNumbners3 = (a, b) => (a + b);      //also valid
+const addTwoNumbners3 = (a, b) => a + b;
+let sum3 = addTwoNumbners3(6, 4);
+console.log(sum3);
 
 // Implicit Returns
+const saySomething = (message) => console.log(message);
+saySomething("halo to ja, pedze do Ciebie, swiatlowodem");
+
+const sayHello = () => console.log("Hello there");
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiline string!
+    </p>`
+)
+
+console.log(returnMultipleLines())
---
+++ excercise1.js
@@ -0,0 +1,37 @@
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'maths','english', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', english: 'Joan', art: 'Simon'},
+        results: {science: 93, maths: 95, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 79, art: 95},
+    }
+];
+
+const averagePoints = (arr, subject) => {
+    let totalMarks = 0;
+    let studentsNumber = 0;
+
+    arr.forEach(student => {
+        if (subject in student.results) {
+            totalMarks += student.results[subject];
+            studentsNumber += 1;
+        }
+    })
+
+    return totalMarks / studentsNumber;
+}
+
+let averageMarks = averagePoints(students, "maths")
+console.log(averageMarks)
